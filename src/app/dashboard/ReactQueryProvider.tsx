77ac1506c4a0200f8a76bd6c2d3841f4d7a089bc
Max Nichols <allenmaxnichols@gmail.com>
2025-09-21 14:51:06 +0300

    one retry

--- src/app/dashboard/ReactQueryProvider.tsx
+++ src/app/dashboard/ReactQueryProvider.tsx
@@ -4,6 +4,6 @@ import { ReactNode, useState } from "react";
 
 export default function ReactQueryProvider({ children }: { children: ReactNode }) {
   // Ensure the QueryClient is only created once per provider instance
-  const [client] = useState(() => new QueryClient());
+  const [client] = useState(() => new QueryClient({ defaultOptions: { queries: { retry: 1 } },}));
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
