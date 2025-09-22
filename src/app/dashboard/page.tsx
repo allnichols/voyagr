a813673abffa766df433558a89ae30ecfebd2d39
Max Nichols <allenmaxnichols@gmail.com>
2025-09-22 17:38:58 +0300

    added search page and google maps

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,18 +3,36 @@
 export default function ItineraryPage() {
 
     return (
-            <div className="relative font-sans grid grid-cols-1 md:grid-cols-[350px_1fr] lg:grid-cols-[450px_1fr] min-h-screen">
-                {/* Sidebar */}
-                {/* <Sidebar /> */}
-                {/* Map */}
-                {/* <TripDetails /> */}
-                {/* <main className="flex flex-col bg-base-200 gap-8 items-start">
-                    <Map />
-                </main> */}
-                {/* Optional Right Panel */}
-                {/* <section className="hidden lg:block bg-base-100 p-4 rounded-lg shadow-md h-full"> */}
-                    {/* Right panel content (e.g., notifications, details) */}
-                {/* </section> */}
+        <div className="flex h-screen">
+            <div className="w-1/2 p-6 overflow-y-auto">
+                <div className="flex flex-col mb-6">
+
+                    <h2 className="text-2xl font-semibold">Explore</h2>
+                    <div className="mt-4">
+                        <label className="input input-lg">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <circle cx="11" cy="11" r="8"></circle>
+                                    <path d="m21 21-4.3-4.3"></path>
+                                </g>
+                            </svg>
+                            <input type="search primary-content" required placeholder="Search" />
+                        </label>
+                    </div>
+                </div>
             </div>
+            {/* map placeholder */}
+            <div className="flex-1 bg-base-300 flex items-center justify-center">
+                <div className="mockup-window border bg-base-100 w-11/12 h-5/6 flex items-center justify-center">
+                    <span className="text-lg text-base-content">Map Placeholder</span>
+                </div>
+            </div>
+        </div>
     );
 }
