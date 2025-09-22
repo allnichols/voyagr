897b83b53ae727b750d7751005f21c1fa5290478
Max Nichols <allenmaxnichols@gmail.com>
2025-09-22 10:45:02 +0300

    add search icon and menu item

--- src/app/dashboard/components/Sidebar/Components/Menu/index.tsx
+++ src/app/dashboard/components/Sidebar/Components/Menu/index.tsx
@@ -4,6 +4,15 @@ import { usePathname } from "next/navigation";
 
 const menuItems = [
     {
+        name: "Search",
+        href: "/dashboard",
+        icon: (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+            </svg>
+        )
+    },
+    {
         name: "Trips",
         href: "/dashboard/trips",
         icon: (
@@ -21,7 +30,7 @@ const menuItems = [
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         )
-    }
+    },
 ]
 
 export default function Menu() {
@@ -35,8 +44,8 @@ export default function Menu() {
 
                             href={item.href}
                             className={`menu-item  ${pathname === item.href ? "bg-gray-300 font-medium" : ""}`}>
-                                {item.icon}
-                                {item.name}
+                            {item.icon}
+                            {item.name}
                         </Link>
                     </li>
                 )
