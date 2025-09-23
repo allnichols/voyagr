dab0e0e96d6a0a70beae34bfd41ae363af214eaa
Max Nichols <allenmaxnichols@gmail.com>
2025-09-23 16:19:05 +0300

    remove unused imports

--- src/app/dashboard/trips/[tripId]/page.tsx
+++ src/app/dashboard/trips/[tripId]/page.tsx
@@ -4,45 +4,8 @@ import { useParams } from "next/navigation";
 import { getTripDetails } from "./actions";
 import Day from "./components/Day";
 import 'leaflet/dist/leaflet.css';
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from "react";
 import Map from "./components/Map";
 
-const MapContainer = dynamic(
-    () => import('react-leaflet').then((mod) => mod.MapContainer),
-    { ssr: false }  // Disable server-side rendering (Leaflet hates SSR)
-);
-
-const TileLayer = dynamic(
-    () => import('react-leaflet').then((mod) => mod.TileLayer),
-    { ssr: false }
-);
-
-const Marker = dynamic(
-    () => import('react-leaflet').then((mod) => mod.Marker),
-    { ssr: false }
-);
-
-
-const Popup = dynamic(
-    () => import('react-leaflet').then((mod) => mod.Popup),
-    { ssr: false }
-);
-
-const svgString = encodeURIComponent(`
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="16" cy="16" r="14" fill="#2563eb" stroke="white" stroke-width="4"/>
-    <text x="16" y="21" text-anchor="middle" fill="white" font-size="16" font-family="Arial" dy=".3em">📍</text>
-  </svg>
-`);
-
-// const svgIcon = new L.Icon({
-//     iconUrl: `data:image/svg+xml,${svgString}`,
-//     iconSize: [32, 32],
-//     iconAnchor: [16, 32],
-//     popupAnchor: [0, -32],
-//     className: "",
-// });
 
 export default function TripPage() {
     const params = useParams();
