b015f1e8dcb5ea677a610020822bc66f0b958071
Max Nichols <allenmaxnichols@gmail.com>
2025-09-25 17:51:58 +0300

    get trip days with tripId

---
+++ src/app/api/trip-days/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const tripId = searchParams.get("tripId")
+
+    try {
+        
+        if (!tripId) {
+            return new Response(JSON.stringify({ error: 'No Trip found ' }))
+        }
+
+        let tripDays = await prisma.tripDay.findMany({
+            where: { tripId: Number(tripId) }
+        })
+
+        return new Response(JSON.stringify(tripDays), { status: 200 });
+    } catch {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }))
+    }
+}
