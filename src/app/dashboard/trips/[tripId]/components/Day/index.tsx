23ff31e90253028ca4f5f940c4284a323ee398cd
Max Nichols <allenmaxnichols@gmail.com>
2025-09-28 14:20:48 +0300

    add ay menu and props

--- src/app/dashboard/trips/[tripId]/components/Day/index.tsx
+++ src/app/dashboard/trips/[tripId]/components/Day/index.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import { useState, memo } from "react";
 import { deleteActivity } from "../../actions"; // Adjust the path as needed
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useToastMutation } from "@/app/dashboard/hooks/useToastMutation";
 import { useCurrentDay } from "@/app/dashboard/store/currentDay";
 import AddActivityBtn from "./AddActivityBtn";
+import DayMenu from "./DayMenu";
 
 async function getTripDayActivites(tripDayId: number | null) {
     const url = `/api/trip-activities${tripDayId !== null ? `?tripDayId=${tripDayId}` : ''}`;
@@ -13,7 +14,7 @@ async function getTripDayActivites(tripDayId: number | null) {
     return res.json();
 }
 
-export const DayDropdown = React.memo(
+export const DayDropdown = memo(
     function DayDropdown({
         dayId,
         dayNumber,
@@ -29,7 +30,6 @@ export const DayDropdown = React.memo(
     }) {
         const setCurrentDay = useCurrentDay((state) => state.setCurrentDay);
         const currentDay = useCurrentDay((state) => state.currentDay.id);
-        const [openDays, setOpenDays] = useState<number[]>([]);
         const queryClient = useQueryClient();
 
         const { data: activities } = useQuery({
@@ -46,29 +46,22 @@ export const DayDropdown = React.memo(
             }
         })
 
-        console.log(activities)
-
         const toast = useToastMutation(deleteActivityMutation, 3000);
 
         const handleDeleteActvity = (activityId: number) => {
             deleteActivityMutation.mutate(activityId);
-        }
-
-        const handleSetOpenDays = (dayNumber: number) => {
-            setOpenDays((prev) => {
-                return prev.includes(dayNumber)
-                    ? prev.filter(d => d !== dayNumber)
-                    : [...prev, dayNumber];
-            })
-        }
+        };
 
         const handleSelectDay = (currentDay: number, dayId: number) => {
-            console.log('select day', dayId);
             if (currentDay !== dayId) {
                 setCurrentDay(dayId);
             }
+        };
+
+        const handleRemoveDay = (dayId: number) => {
+            // Implement the logic to remove the day here
+            console.log("Remove day with ID:", dayId);
         }
-;
 
         return (
             <>
@@ -105,25 +98,7 @@ export const DayDropdown = React.memo(
                             </div>
 
                         </div>
-                        <div className="mr-2">
-                            <div className="dropdown dropdown-top dropdown-left z-100">
-                                <div tabIndex={index} role="button" className="btn btn-ghost btn-circle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                                    </svg>
-                                </div>
-                                <ul tabIndex={index} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-xl/20">
-                                    <li>
-                                        <a>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                            </svg>
-                                            Remove
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        <DayMenu index={index} dayId={dayId} />
                     </div>
                     <div
                         className="ml-4 mb-4 transition-all duration-300"
@@ -131,7 +106,6 @@ export const DayDropdown = React.memo(
                             maxHeight: isOpen ? "500px" : "0",
                             opacity: isOpen ? 1 : 0,
                             display: isOpen ? "block" : "none",
-                            // overflow: "hidden",
                         }}
                     >
 
