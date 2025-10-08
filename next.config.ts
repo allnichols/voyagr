cd2c47cfb19d47a284b2cd6525b1a36ea833b216
Max Nichols <allenmaxnichols@gmail.com>
2025-10-08 15:06:32 +0300

    add google uri for icons to config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // "https://maps.gstatic.com/mapfiles/place_api/icons/v2/cafe_pinlet"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.gstatic.com',
+        port: '',
+        pathname: '/mapfiles/**',
+      }
+     ]
+  }
 };
 
 export default nextConfig;
