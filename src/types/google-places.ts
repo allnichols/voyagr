ba07340e98a0da9349bfcdd2e1d76da104942068
Max Nichols <allenmaxnichols@gmail.com>
2025-10-08 15:06:05 +0300

    add google place type

---
+++ src/types/google-places.ts
@@ -0,0 +1,33 @@
+export interface GooglePlace {
+  id: string;
+  displayName: {
+    text: string;
+    languageCode: string;
+  };
+  formattedAddress: string;
+  location: Location;
+  priceLevel?: PriceLevel;
+  types: string[];
+  rating?: number;
+  iconMaskBaseUri?: string;
+  iconBackgroundColor?: string;
+}
+
+export type Location = {
+  latitude: number;
+  longitude: number;
+};
+
+export enum PriceLevel {
+  UNSPECIFIED = "PRICE_LEVEL_UNSPECIFIED",
+  FREE = "PRICE_LEVEL_FREE",
+  INEXPENSIVE = "PRICE_LEVEL_INEXPENSIVE",
+  MODERATE = "PRICE_LEVEL_MODERATE",
+  EXPENSIVE = "PRICE_LEVEL_EXPENSIVE",
+  VERY_EXPENSIVE = "PRICE_LEVEL_VERY_EXPENSIVE",
+}
+
+export interface GooglePlacesResponse {
+  query: string;
+  places: GooglePlace[];
+}
