91cb6cf24c8922035de37a70dc6621ba8e1adcf3
Max Nichols <allenmaxnichols@gmail.com>
2025-10-14 17:16:05 +0300

    add css for popover and btn for details

--- src/app/features/dashboard/Trips/components/Map/index.tsx
+++ src/app/features/dashboard/Trips/components/Map/index.tsx
@@ -5,7 +5,9 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useCurrentDay } from "@/app/features/dashboard/store/currentDay";
 import ErrorBoundary from "@/app/utils/ErrorBoundry";
+import { TripActivity } from "@prisma/client";
 import "leaflet/dist/leaflet.css";
+import "./popup.css";
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
@@ -27,10 +29,9 @@ const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
 });
 
 const svgString = encodeURIComponent(`
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="16" cy="16" r="14" fill="#2563eb" stroke="white" stroke-width="4"/>
-    <text x="16" y="21" text-anchor="middle" fill="white" font-size="16" font-family="Arial" dy=".3em">📍</text>
-  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#422ad5" class="size-5">
+    <path fill-rule="evenodd" d="m9.69 18.933.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 0 0 .281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 1 0 3 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 0 0 2.273 1.765 11.842 11.842 0 0 0 .976.544l.062.029.018.008.006.003ZM10 11.25a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z" clip-rule="evenodd" />
+</svg>
 `);
 
 async function getTripActivities(
@@ -117,22 +118,16 @@ export default function Map() {
         />
         {icon &&
           data &&
-          data.map((activity: any) => {
+          data.map((activity: TripActivity) => {
             return (
               <Marker
                 key={activity.id}
                 icon={icon}
-                position={[activity.latitude, activity.longitude]}
+                position={[activity.latitude ?? 0, activity.longitude ?? 0]}
               >
                 <Popup>
                   <div>
-                    <h3 className="font-bold">{activity.place}</h3>
-                    <button
-                      className="btn btn-xs btn-primary mt-2 rounded-2xl"
-                      type="button"
-                    >
-                      View More
-                    </button>
+                    <button className="btn btn-sm btn-ghost">Details</button>
                   </div>
                 </Popup>
               </Marker>
