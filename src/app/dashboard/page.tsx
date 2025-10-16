cb10d3d68253def047f052820436349ee0e10fae
Max Nichols <allenmaxnichols@gmail.com>
2025-10-16 16:36:03 +0300

    restructure pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,53 +1,13 @@
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import "leaflet/dist/leaflet.css";
+import Trips from "@/app/features/dashboard/trips";
+import { Suspense } from "react";
 
-export default async function MainPage() {
+export default function TripsPage() {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 p-6 overflow-y-auto">
-        <div className="flex flex-col mb-6 w-full">
-          <h2 className="text-2xl font-semibold">Explore</h2>
-          <div className="mt-4">
-            <label className="input input-lg w-[85%]">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <title>Search</title>
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <path d="m21 21-4.3-4.3"></path>
-                </g>
-              </svg>
-              <input
-                type="search primary-content"
-                required
-                placeholder="Search"
-              />
-            </label>
-          </div>
-        </div>
-      </div>
-      {/* map placeholder */}
-      <div className="w-1/2 flex items-center justify-center rounded-md overflow-y-hidden">
-        {/* <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          style={{ height: "95%", width: "95%", borderRadius: "16px" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer> */}
+    <div className="p-4">
+      <div>
+        <Suspense fallback={<div>Loading trips...</div>}>
+          <Trips />
+        </Suspense>
       </div>
     </div>
   );
