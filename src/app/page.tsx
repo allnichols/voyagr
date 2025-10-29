d3da218fe45035f6683f5b1a61a82abbe6c72a4e
Max Nichols <allenmaxnichols@gmail.com>
2025-10-29 16:17:37 +0300

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
 import NavBar from "@/app/components/marketing/navbar";
 import Hero from "@/app/components/marketing/hero";
 import Features from "./components/marketing/features";
