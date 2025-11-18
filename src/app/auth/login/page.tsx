7c0d75109076462b405edc5ad4107780cacc205b
Max Nichols <allenmaxnichols@gmail.com>
2025-11-18 11:41:12 +0200

    wrap loginform in suspense

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import LoginForm from "@/app/features/auth/login/login-form";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Suspense>
+      <LoginForm />
+    </Suspense>
+  );
 }
