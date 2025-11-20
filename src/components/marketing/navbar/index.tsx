8c1dec9e1de3936f2af221bd89f07d9d6a01d463
Max Nichols <allenmaxnichols@gmail.com>
2025-11-20 13:40:54 +0200

    add divider and login to menu

--- src/components/marketing/navbar/index.tsx
+++ src/components/marketing/navbar/index.tsx
@@ -4,18 +4,26 @@ export default function NavBar() {
   return (
     <div className="navbar container-custom">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Voyagr</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <Link
+              href={"/auth/login"}
+              className="btn btn-outline btn-sm btn-info rounded-2xl"
+            >
+              Login
+            </Link>
           </li>
+          <div className="divider divider-horizontal" />
           <li>
-            <a>Link 2</a>
-          </li>
-          <li>
-            <Link href={"/auth/login"} className="btn btn-outline btn-sm btn-info rounded-2xl">Login</Link>
+            <Link
+              href={"/auth/signup"}
+              className="btn btn-outline btn-sm btn-accent rounded-2xl"
+            >
+              Signup
+            </Link>
           </li>
         </ul>
       </div>
