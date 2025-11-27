2280a30f151fde0bb24008cdc7743b9690139ead
Max Nichols <allenmaxnichols@gmail.com>
2025-11-27 14:05:36 +0200

    add view details to menu

--- src/features/dashboard/trips/components/day/activity/menu/index.tsx
+++ src/features/dashboard/trips/components/day/activity/menu/index.tsx
@@ -1,5 +1,5 @@
-import DetailsPanel from "../../../details-panel";
-
+import DetailsPanel from "../../../../../details-panel";
+import { useDetailsDrawer } from "@/features/dashboard/store/detailsDrawer";
 
 type ActivityMenuProps = {
     activityIndex: number;
@@ -8,6 +8,8 @@ type ActivityMenuProps = {
 }
 
 export default function ActivityMenu({ activityIndex, activityId, handleDeleteActivity }: ActivityMenuProps) {
+    const toggleDetailsDrawer = useDetailsDrawer((state) => state.toggleDetailsDrawer);
+    
     return (
         <div className="dropdown dropdown-top dropdown-left">
             <div tabIndex={activityIndex} className="btn btn-ghost btn-circle">
@@ -51,8 +53,24 @@ export default function ActivityMenu({ activityIndex, activityId, handleDeleteAc
                         Delete
                     </a>
                 </li>
-                <li>
-                    <DetailsPanel />
+                <li className="p-2 font-semibold">
+                    <a onClick={() => toggleDetailsDrawer()}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-4"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                            />
+                        </svg>
+                        View Details
+                    </a>
                 </li>
             </ul>
         </div>
