d93307fcda5fd8c36f3b1dc443f61f9f971b382b
Max Nichols <allenmaxnichols@gmail.com>
2025-12-02 15:19:21 +0200

    add topbar

--- src/features/dashboard/sidebar/index.tsx
+++ src/features/dashboard/sidebar/index.tsx
@@ -1,11 +1,15 @@
 import Menu from "./components/menu";
+import TopBar from "./components/top-bar";
 
 export default function Sidebar({ children }: { children: React.ReactNode }) {
   //   add very light grey to background then refactor how the tirps cards look
   return (
     <div className="flex h-full">
       <Menu />
+
       <main className="flex-1 p-1 overflow-y-hidden">
+        <TopBar />
+
         <div className="md:pt-0 pt-10">{children}</div>
       </main>
     </div>
