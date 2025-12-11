63496c704c1f22afa71505053423bed8819ab9dc
Max Nichols <allenmaxnichols@gmail.com>
2025-12-11 15:44:21 +0200

    change file name

---
+++ src/features/dashboard/create-trip/loading-overlay.tsx
@@ -0,0 +1,7 @@
+export default function LoadingOverlay() {
+  return (
+    <div className="mt-8 p-4 border rounded bg-gray-50">
+      <pre className="whitespace-pre-wrap">Loading...</pre>
+    </div>
+  );
+}
